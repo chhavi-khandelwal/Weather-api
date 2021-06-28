e64afa327da1c3dc64a41b8e5062dee7c3fe40b6
Chhavi Khandelwal <chhavi.kh09@gmail.com>
2021-06-28 16:17:43 +0200

    chore(components): Add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export type Props = {
+  type?: string;
+  disabled?: boolean;
+  changeListener?: Function;
+  error?: string;
+  placeholder?: string;
+  name?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+  const {
+    disabled,
+    placeholder,
+    error,
+    changeListener = () => {},
+    type,
+    name,
+  } = props;
+
+  const onTextChange = (event: { target: HTMLInputElement }) => {
+    const { value } = event.target;
+    changeListener(value);
+  };
+
+  return (
+    <Styled.InputContainer>
+      <Styled.Input
+        type={type}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onTextChange}
+        name={name}
+        ref={ref}
+        error={error}
+      />
+      {error && <Styled.ErrorContainer>{error}</Styled.ErrorContainer>}
+    </Styled.InputContainer>
+  );
+});
+
+const Styled = {
+  InputContainer: styled.div`
+    position: relative;
+  `,
+  Input: styled.input<{ error?: string }>`
+    width: 150px;
+    height: 38px;
+    font-size: 12px;
+    padding: ${(props) => props.theme.spacings.s};
+    color: ${(props) => props.theme.colors.black90};
+    border-radius: 10px;
+    border: 1px solid
+      ${(props) =>
+        props.error ? props.theme.colors.pinkDark : props.theme.colors.black20};
+  `,
+  ErrorContainer: styled.span`
+    font-size: 10px;
+    color: red;
+    position: absolute;
+    left: 10px;
+    bottom: -15px;
+  `,
+};
+
+export default Input;
